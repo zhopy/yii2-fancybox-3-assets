1a5f45273c8c49b1ee8939d3fe9fcea2fcbd9385
zhopy <42891408+zhopy@users.noreply.github.com>
2019-06-10 15:19:53 +0800

    Create FancyBoxAsset.php

---
+++ FancyBoxAsset.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017 Newerton Vargas de Araújo
+ * @link http://newerton.com.br
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+namespace zhopy\fancybox3;
+
+use yii\web\AssetBundle;
+
+class FancyBoxAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/fancybox';
+    public $css = [
+        'dist/jquery.fancybox' . (!YII_DEBUG ? '.min' : '') . '.css'
+    ];
+    public $js = [
+        'dist/jquery.fancybox' . (!YII_DEBUG ? '.min' : '') . '.js'
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+} 
